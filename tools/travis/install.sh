f0101ca42e7fe6704dfea8da27bc86e70ae0fbb9
Pradyun Gedam <pradyunsg@gmail.com>
2018-05-12 16:48:12 +0530

    Rework CI configuration

--- tools/travis/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-git config --global user.name "Travis CI"
-git config --global user.email "travis@travis-ci.org"
-
-pip install --upgrade setuptools
-pip install --upgrade tox
